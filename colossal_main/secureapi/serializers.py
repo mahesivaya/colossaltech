3d4515c4047e9c4d5f93f44f836b92448d4d01a7
mahesivaya <mahesivaya@protonmail.com>
2024-04-18 13:42:57 -0400

    Deleted to create later

--- colossal_main/secureapi/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import SecdataModel
-
-
-class DatasecSerialize(serializers.ModelSerializer):
-    class Meta:
-        model = SecdataModel
-        fields = '__all__'
