9cf2ea2ff76c3d2da5b2667d4dd01ea922353edc
mahesivaya <167259736+mahesivaya@users.noreply.github.com>
2024-10-22 13:56:06 -0400

    test database

--- colossal_main/colossal_main/settings.py
+++ colossal_main/colossal_main/settings.py
@@ -75,24 +75,24 @@ WSGI_APPLICATION = 'colossal_main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'colossaltechdatabase',
-        'USER': 'postgres',
-        'PASSWORD': 'Jaimatadi@3',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'colossaltech',
+#         'USER': 'postgres',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
